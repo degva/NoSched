bfc9910e9a5e4e0de983f261307a170ab597e1cd
Diego Vargas <degva.29@gmail.com>
2014-10-26 19:59:36 -0500

    Added functionality. But needs to be cleaned!!

--- nosched/www/js/main.js
+++ nosched/www/js/main.js
@@ -1,3 +1,6 @@
+/* Some ideas:
+ * Maybe the user wants multitasking.
+ */
 $(document).ready(function() {
 	// For the toggle thing
 	$('[data-toggle]').click(function() {
@@ -6,6 +9,8 @@ $(document).ready(function() {
 	});
 
 
+	// This is for the timer thingy...
+	//
 	var now = new Date()
 	// How much time until we get to sunday?
 	var nextSun = 6 - now.getDay();
@@ -16,6 +21,9 @@ $(document).ready(function() {
 
 	// Execute timer every second
 	var counter = setInterval(timer, 1000);
+
+	// This is for the progress only!!!
+	var actprg = 0;
 
 	function timer() {
 		Seconds -= 1 ;
@@ -29,5 +37,35 @@ $(document).ready(function() {
 			Hours -= 1;
 			Minutes = 60;
 		}
+		actprg.val(actprg.val() + 1)
 	}
+	// Timer thingy end
+
+	// This is for the 'add task/cancel button'
+	function showAdd() {
+		$('.add').toggleClass('rotated');
+		$('.addtask').toggleClass('hide');
+	}
+	$('.add').click(function(){
+		showAdd()
+	});
+	$('.button.warning').click(function() {
+		showAdd()
+	})
+
+	// When we start or get done a task:
+	$('.done').click(function() {
+		// Ask if they are sure (maybe just want to pause!) or maybe add more time!
+		// Finish the task! :D
+		console.log('finito');
+	});
+	$('.startTask').click(function() {
+		$(this).toggleClass('hide');
+		var parent = $(this).parent()
+		parent.find('.done').toggleClass('hide');
+		parent.find('progress').toggleClass('hide');
+		// now check if the parent has one or another in hide
+		var obj = parent.find('progress');
+		actprg = obj;
+	});
 });
